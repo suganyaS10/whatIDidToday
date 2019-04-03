69ca351dfb9de510874d60b3d7e6a1186cf6fc58
ssuganya <ssuganya@collab.net>
2019-04-03 14:53:16 +0530

    Added custom attribute name to Devise users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
 
+	 before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name ])
+    #in keys you list all the input you want to accept.
+  end
+
 end
