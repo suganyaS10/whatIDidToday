9311c1bc69494d80af23b1601cd09248db6ea441
ssuganya <ssuganya@collab.net>
2019-04-03 15:28:34 +0530

    Added support for hour/minute/seconds for posted time

--- app/models/task.rb
+++ app/models/task.rb
@@ -16,7 +16,7 @@ class Task < ApplicationRecord
 			task_hash[:id] = task.id
 			task_hash[:description] = task.description
 			task_hash[:creator] = task.creator.name
-			task_hash[:created_at] = ((Time.now - task.created_at) / 1.hour).round
+			task_hash[:created_at] = task.calculated_posted_ago_time
 			user_likes = task.likes.select {|like| like.user_id == current_user.id}
 			task_hash[:likes_count] = task.likes.present? ? task.likes.count : 0
 			task_hash[:comments_count] =  task.comments.present? ? task.comments.count : 0
@@ -27,5 +27,22 @@ class Task < ApplicationRecord
 			tasks_array << task_hash
 		end
 		tasks_array
+	end
+
+	def calculated_posted_ago_time
+		posted_ago_time = ''
+		 posted_ago_hours = ((Time.now - self.created_at) / 1.hour).round
+		 posted_ago_minutes = ((Time.now - self.created_at) / 1.minute).round
+
+		 if posted_ago_hours > 0
+		   posted_ago_time = posted_ago_hours.to_s + ' hours'
+		 elsif posted_ago_minutes > 0
+		   posted_ago_time = posted_ago_minutes.to_s + ' minutes'
+		 else
+		   posted_ago_seconds = ((Time.now - self.created_at) / 1.second).round
+		   posted_ago_time = posted_ago_seconds.to_s + ' seconds'
+		 end
+
+		 posted_ago_time
 	end
 end
